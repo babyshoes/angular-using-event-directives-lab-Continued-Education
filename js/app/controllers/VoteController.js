31ac55ae6fc5473c02b7dff4800c2a7c496cdc7d
babyshoes <dianecai@gmail.com>
2016-04-03 15:59:10 -0400

    done

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,20 @@
 function VoteController() {
     this.votes = 0;
+    var voted;
+    var decremented;
 
     this.incrementVotes = function () {
-
+      if(!voted){
+        this.votes++;
+        voted = true;
+      }
     };
 
     this.decrementVotes = function () {
-
+      if(!decremented){
+        this.votes--
+        decremented = true;
+      }
     };
 }
 
